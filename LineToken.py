23becaa81eb6a32cc42ccb2338a625da96c71986
nakamura0067 <nakamura0067@gmail.com>
2024-04-11 01:05:54 +0900

    resolve issue-3

--- LineToken.py
+++ LineToken.py
@@ -10,25 +10,26 @@ ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN")
 db_connect = sqlite3.connect('sqlite_test.db')
 db_curs = db_connect.cursor()
 
-dt_now = datetime.datetime.now().strftime('%Y/%m/%d %H:%M')
+dt_now = datetime.datetime.now() 
+dt_pre = dt_now + datetime.timedelta(days=-7)
 
-# データ参照の命令をSQLにて指示
-sql = 'SELECT * FROM human_sensor where high_date = "'+ dt_now + '"'
+# 一週間前のデータを取得する
+sql = 'SELECT * FROM human_sensor where high_date > "'+ dt_pre.strftime('%Y/%m/%d') + '"'
 db_curs.execute(sql)
-human_sensor_count = db_curs.fetchone()
+human_sensor_list = db_curs.fetchall()
 
-if human_sensor_count is None:
-     sql = 'INSERT INTO human_sensor(high_date) values ("' + dt_now + '")'
+if human_sensor_list is None:
+     headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
+     data = {"message": "私は死んだ！\n↓この時にはだいたい死んでる\n" + dt_now.strftime('%Y/%m/%d %H:%M')}
+
+     requests.post(
+          "https://notify-api.line.me/api/notify",
+          headers=headers,
+          data=data,
+     )
+else:
+     sql = 'INSERT INTO human_sensor(high_date) values ("' + dt_now.strftime('%Y/%m/%d %H:%M') + '")'
      db_curs.execute(sql)
 
 db_connect.commit()
 db_connect.close()
-
-headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
-data = {"message": "私は死んだ！\n↓この時にはだいたい死んでる\n" + dt_now}
-
-requests.post(
-     "https://notify-api.line.me/api/notify",
-     headers=headers,
-     data=data,
-)
