42ef5a9b159ce3576bedaa452728696efe6358ab
nakamura0067 <nakamura0067@gmail.com>
2024-04-11 00:23:50 +0900

    resolve issue-3

--- LineToken.py
+++ LineToken.py
@@ -1,15 +1,31 @@
 import requests
 import datetime
 import os
+import sqlite3
 from dotenv import load_dotenv
 
 load_dotenv()
 ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN")
 
-headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
+db_connect = sqlite3.connect('sqlite_test.db')
+db_curs = db_connect.cursor()
 
-dt_now = datetime.datetime.now()
-data = {"message": "\n現在の時間は" + dt_now.strftime('%Y年%m月%d日 %H:%M:%S') + "です。"}
+dt_now = datetime.datetime.now().strftime('%Y/%m/%d %H:%M')
+
+# データ参照の命令をSQLにて指示
+sql = 'SELECT * FROM human_sensor where high_date = "'+ dt_now + '"'
+db_curs.execute(sql)
+human_sensor_count = db_curs.fetchone()
+
+if human_sensor_count is None:
+     sql = 'INSERT INTO human_sensor(high_date) values ("' + dt_now + '")'
+     db_curs.execute(sql)
+
+db_connect.commit()
+db_connect.close()
+
+headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
+data = {"message": "私は死んだ！\n↓この時にはだいたい死んでる\n" + dt_now}
 
 requests.post(
      "https://notify-api.line.me/api/notify",
